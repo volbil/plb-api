b7f814df42da57f63c3af74de42919e7c5fa5940
volbil <volbilnexus@gmail.com>
2022-10-11 02:05:00 +0300

    Make mempool refresh faster

--- sync.py
+++ sync.py
@@ -5,5 +5,5 @@ from server.sync import sync_mempool
 background = BlockingScheduler()
 background.add_job(sync_tokens, "interval", seconds=30)
 background.add_job(sync_mempool, "interval", seconds=5)
-background.add_job(sync_blocks, "interval", seconds=5)
+background.add_job(sync_blocks, "interval", seconds=1)
 background.start()
