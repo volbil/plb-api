84b8519dfbb1fa814f494a5cdee9abb76224c947
volbil <volbilnexus@gmail.com>
2022-10-12 18:03:37 +0300

    Update sync time

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ from server.sync import sync_blocks, sync_tokens
 from server.sync import sync_mempool
 
 background = BlockingScheduler()
+background.add_job(sync_mempool, "interval", seconds=30)
 background.add_job(sync_tokens, "interval", seconds=30)
-background.add_job(sync_mempool, "interval", seconds=5)
-background.add_job(sync_blocks, "interval", seconds=1)
+background.add_job(sync_blocks, "interval", seconds=5)
 background.start()
